dfe9adf89bd08ecb5599ff62b3e8e1da0ff86435
Safiullah <safiullahsaleem820@gmail.com>
2024-11-02 14:20:51 +0500

    new iteration

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,23 @@
+'use client'
+
 import './globals.css'
 import React from 'react'
 import Home from '@/components/pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Contact from '@/components/pages/contact';
+import About from '@/components/pages/About';
+import Services from '@/components/pages/Services';
 
 const page = () => {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/Contact' element={<Contact />} />
+        <Route path='/Services' element={<Services />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import styles from "./Layout.module.css";
+import Navbubble from "./Navbubble";
 
 const Layout = ({ children }) => {
   return (
     <div className={styles.desktop}>
       <Header />
       <main className={styles.mainContent}>{children}</main>
+      <Navbubble />
       <Footer />
     </div>
   );
 };
 
 export default Layout;
+  
